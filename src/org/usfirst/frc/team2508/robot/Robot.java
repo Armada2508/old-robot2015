c3861e2424322453d945f16cac4fcc4207ef22d7
Robotics Computer <armada.robotics2508@gmail.com>
2015-02-06 14:39:48 -0600

    Encoder & started image processing.

--- src/org/usfirst/frc/team2508/robot/Robot.java
+++ src/org/usfirst/frc/team2508/robot/Robot.java
@@ -1,8 +1,10 @@
 
 package org.usfirst.frc.team2508.robot;
 
-
 import java.util.Date;
+
+import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.Image;
 
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Compressor;
@@ -20,28 +22,36 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Robot extends SampleRobot {
 	
 	// Robot
-	Talon talon1 = new Talon(5);
-	Talon talon2 = new Talon(6);
-
+	Talon talon = new Talon(4);
+	
     RobotDrive chassis = new RobotDrive(0, 1, 2, 3);
-    LogitechGamepad gamePad = new LogitechGamepad();
+    LogitechGamepad gamepad = new LogitechGamepad();
     Solenoid pneumatic0 = new Solenoid(0);
     Solenoid pneumatic1 = new Solenoid(1);
     Relay relayLight = new Relay(0);
     Compressor compressor = new Compressor(1);
+    Encoder encoder = new Encoder(0, 1);
+    
+    // Camera
     CameraServer camera = CameraServer.getInstance();
+    Image image = null;
    
     // System
     Date lastSolenoidEnable = new Date();
     
     // Variables
-    double speedFactor = 1.0;
-    double rotationSpeed = 0.3;   
+    double speedFactor = 1.0; // multiplier for directional speed
+    double rotationSpeed = 0.3;  // multiplier for rotation speed
+	double wheelCircumference = 5.0; // circumference in meters of encoded wheels
 
     public Robot() {
     	// Setup camera
         camera.setQuality(50);
-        camera.startAutomaticCapture("cam1");
+        image = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+        int session = NIVision.IMAQdxOpenCamera("cam1", NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+    	NIVision.IMAQdxConfigureGrab(session);
+    	NIVision.IMAQdxStartAcquisition(session);
+    	camera.setImage(image);
         
         // Setup chassis
         chassis.setInvertedMotor(MotorType.kFrontRight, true);
@@ -60,6 +70,24 @@ public class Robot extends SampleRobot {
         
         while (isOperatorControl() && isEnabled()) {
         	//-------------------------------------------------------------
+        	// Encoder
+        	//-------------------------------------------------------------
+        	// Measures the number of rotations of a wheel.
+        	// getRaw() of 2000 is approximately one rotation
+        	{
+        		int encoderValue = encoder.getRaw();
+
+            	if (Math.abs(encoderValue) >= 1940) {
+            		talon.set(0);
+            	}
+            	
+            	if (gamepad.getFirstPressY()) {
+            		encoder.reset();
+            		talon.set(1);
+            	}
+        	}
+        	
+        	//-------------------------------------------------------------
         	// Pneumatic Piston Control Using Solenoid
         	//-------------------------------------------------------------
         	// Using a 2-way solenoid, in order for pneumatic to extend, pneumatic0 
@@ -70,7 +98,7 @@ public class Robot extends SampleRobot {
     			Date now = new Date();
         		int timeSince = (int) (now.getTime() - lastSolenoidEnable.getTime());
         		
-        		if (gamePad.getButtonRB() == true && timeSince > 1000) {
+        		if (gamepad.getButtonRB() == true && timeSince > 1000) {
         			pneumatic0.set(true);
         			pneumatic1.set(false);
         			lastSolenoidEnable = now;
@@ -88,15 +116,15 @@ public class Robot extends SampleRobot {
         	{
         		// Pressing 'LT' on gamePad decreases speedFactor by 0.1
         		// Pressing 'RT' on gamePad increases speedFactor by 0.1
-        		if (gamePad.getFirstPressLT())
+        		if (gamepad.getFirstPressLT())
         			speedFactor -= 0.1;
-        		if (gamePad.getFirstPressRT())
+        		if (gamepad.getFirstPressRT())
         			speedFactor += 0.1;
         		
         		// Pressing
-        		if (gamePad.getFirstPressLeftStickPress())
+        		if (gamepad.getFirstPressLeftStickPress())
         			rotationSpeed -= 0.1;
-        		if (gamePad.getFirstPressRightStickPress())
+        		if (gamepad.getFirstPressRightStickPress())
         			rotationSpeed += 0.1;
         		
         		// Keep factor between 0.3 and 1
@@ -110,9 +138,9 @@ public class Robot extends SampleRobot {
         	{
             	// gamePad grabs Y value of thumbstick and multiplies by 
             	// speedFactor to get leftSpeed and rightSpeed
-	        	double xMovement = (gamePad.getLeftStickX() * speedFactor);
-	        	double yMovement = gamePad.getLeftStickY() * speedFactor;
-	        	double rotation = gamePad.getRightStickX() * rotationSpeed;
+	        	double xMovement = (gamepad.getLeftStickX() * speedFactor);
+	        	double yMovement = gamepad.getLeftStickY() * speedFactor;
+	        	double rotation = gamepad.getRightStickX() * rotationSpeed;
 	        	
 	        	// Tank drive at modified speed
 	            // chassis.tankDrive(leftSpeed, rightSpeed);
@@ -124,7 +152,7 @@ public class Robot extends SampleRobot {
         	
         	// Relay light switch
         	{
-        		if (gamePad.getFirstPressX()) {
+        		if (gamepad.getFirstPressX()) {
         			if (relayLight.get() == Value.kOn)
         				relayLight.set(Value.kOff);
         			else
@@ -134,7 +162,7 @@ public class Robot extends SampleRobot {
         	
         	// Compressor 
         	{
-        		if (gamePad.getFirstPressY()) {
+        		if (gamepad.getFirstPressY()) {
         			if (compressor.enabled())
         				compressor.start();
         			else
@@ -146,16 +174,17 @@ public class Robot extends SampleRobot {
         	// Smart Dashboard
         	//-------------------------------------------------------------
         	{
-	        	SmartDashboard.putNumber("Left Stick Y", gamePad.getLeftStickY());
-	        	SmartDashboard.putNumber("Right Stick Y", gamePad.getRightStickY());;
+	        	SmartDashboard.putNumber("Left Stick Y", gamepad.getLeftStickY());
+	        	SmartDashboard.putNumber("Right Stick Y", gamepad.getRightStickY());;
 	        	SmartDashboard.putNumber("Speed Factor", speedFactor);
 	        	SmartDashboard.putNumber("Rotation Factor", rotationSpeed);
         		SmartDashboard.putBoolean("Compressor", compressor.enabled());
 	        	SmartDashboard.putBoolean("Solenoid Status", pneumatic0.get() && !pneumatic1.get());
 	        	SmartDashboard.putBoolean("Relay Light Status", relayLight.get() == Value.kOn);
+	        	SmartDashboard.putNumber("Encoder", encoder.getRaw());
         	}
             
-            gamePad.updatePrevButtonStates();
+            gamepad.updatePrevButtonStates();
             Timer.delay(0.01);
         }
     }
